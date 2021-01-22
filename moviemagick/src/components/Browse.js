dd0d947b00dd78cb4f6531c9d9083713d6bfe99c
stephenvincentibanez <stephenvincentibanez@gmail.com>
2021-01-22 14:15:00 -0800

    refactored card into MovieCard, working out button onHover

--- moviemagick/src/components/Browse.js
+++ moviemagick/src/components/Browse.js
@@ -1,40 +1,15 @@
 import React, {Component} from 'react';
 import Container from 'react-bootstrap/Container'
-import Card from 'react-bootstrap/Card'
+import MovieCard from './MovieCard'
 import Row from 'react-bootstrap/Row'
 
 
 class Browse extends Component {
-
-    state={
-        display: false
-    }
-
-    handleEditUserClick = () => {
-        this.props.history.push('/edit_user')
-    }
-
-    handleOnClick = (movie) => {
-        this.props.handleMovieClick(movie)
-        this.props.history.push(`/movies/${movie.id}`)
-    }
-
-    handleHover = () => {
-        this.setState({
-            display: true
-        })
-    }
     
     renderCards = () => {
         return this.props.movies.map(movie => {
             return(
-            <Card onClick={() => this.handleOnClick(movie)} onMouseEnter={this.handleHover} style={{width: '18rem'}} key={movie.id}>
-                <Card.Img variant="top" src={movie.poster}/>
-                <Card.Body>
-                    <Card.Title> {movie.title} </Card.Title>
-                    <Card.Text> {movie.plot} </Card.Text>
-                </Card.Body>
-            </Card>
+                <MovieCard movie={movie} handleMovieClick={this.props.handleMovieClick}/>
             )
         })
     }
---
+++ moviemagick/src/components/MovieCard.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import { withRouter } from "react-router";
+
+class MovieCard extends Component {
+
+    state={
+        display: false
+    }
+
+    handleOnClick = (movie) => {
+        this.props.handleMovieClick(movie)
+        this.props.history.push(`/movies/${movie.id}`)
+    }
+
+    handleHover = () => {
+        this.setState({
+            display: true
+        })
+    }
+
+    handleUnhover = () => {
+        this.setState({
+            display: false
+        })
+    }
+
+    render() {
+        return (
+            <Card onClick={() => this.handleOnClick(this.props.movie)} onMouseEnter={this.handleHover} onMouseOut={this.handleUnhover} style={{width: '18rem'}} key={this.props.movie.id}>
+                <Card.Img variant="top" src={this.props.movie.poster}/>
+                <Card.Body>
+                    <Card.Title> {this.props.movie.title} </Card.Title>
+                    <Card.Text> {this.props.movie.plot} </Card.Text>
+                    {this.state.display === true ? <Button>Add to Watchlist</Button> : null}
+                </Card.Body>
+            </Card>
+        );
+    }
+}
+
+export default withRouter(MovieCard);
